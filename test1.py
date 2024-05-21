37f5fdd2911a73c7213de534e1944a58132fc4c3
Vojtěch Mautner <vojtulam@email.cz>
2024-05-21 15:41:18 +0200

    komentare u plot

--- test1.py
+++ test1.py
@@ -5,6 +5,6 @@ world = "Universe"
 
 print(np.random.randint(0,10)*np.random.randint(10,20))
 
-plt.plot([int(x**2) for x in range(-20,20)])
-plt.plot([int((x**2)*-1) for x in range(-20,20)])
+plt.plot([int(x**2) for x in range(-20,20)])#plot
+plt.plot([int((x**2)*-1) for x in range(-20,20)])#plot2
 plt.show()
