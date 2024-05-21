c0a48bbd6cc4d1efb775d1ff97a1e52d735c17ca
Vojtěch Mautner <vojtulam@email.cz>
2024-05-21 15:46:24 +0200

    print prvni funkce

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot  as plt
 
-print(np.random.randint(0,10)*np.random.randint(10,20))
+print([int(x**2) for x in range(-20,20)])
 
 plt.plot([int(x**2) for x in range(-20,20)])#plot
 plt.plot([int((x**2)*-1) for x in range(-20,20)])#plot2
