932f472984217aa0485b705a51134767f74aab97
Vojtěch Mautner <vojtulam@email.cz>
2024-05-21 15:28:51 +0200

    pridani prumeru hodnot pole

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ cisla = [0,2,32,2,3,1,31,5]
 for i in hello:
     seznam.append(i)
 
+cisla_sum = sum(cisla)/len(cisla)
+
 print(seznam)
 helloworld = hello.split(" ")
 hello,world = helloworld[0],helloworld[1]
