0a889593bb883c9480b60d37c825d916cad7c7a0
Vojtěch Mautner <vojtulam@email.cz>
2024-05-21 15:23:53 +0200

    uprava splitu

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ for i in hello:
     seznam.append(i)
 
 print(seznam)
-print(hello.split(" "))
+helloworld = hello.split(" ")
+hello,world = helloworld[0],helloworld[1]
+print(hello,world)
 
 if len(hello) < 10: #pokud delka stringu vetsi jak 10
     print("True") 
