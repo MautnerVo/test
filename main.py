72c2825b9c20a57250b3eb3c3cc95845e86eb691
Vojtěch Mautner <vojtulam@email.cz>
2024-05-21 15:18:45 +0200

    separace stringu

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 hello = "Hello World" #string
 
+seznam = []
+
+for i in hello:
+    seznam.append(i)
+
+print(seznam)
+
+
 if len(hello) < 10: #pokud delka stringu vetsi jak 10
     print("True") 
 else:
